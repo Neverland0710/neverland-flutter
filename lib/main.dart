5f26faa3dd14877998f0c7c726d5250d12829511
howon-Jeon <howon0830@naver.com>
2025-06-11 16:20:49 +0900

    Design: 디버그 배너 제거 설정 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:neverland_project/screen/home_screen.dart';  //lib에 있는 파일을 불러옴
+import 'package:neverland_project/screen/home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-        home: HomeScreen()  // import 없이 Show context Action을 이용하면 import 자동완성
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen()
     ),
 
   );
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,5 @@
-// lib 폴더에 폴더 하나 더 만들어 파일 생성
-
 import 'package:flutter/material.dart';
 
-//stless로 자동완성
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
