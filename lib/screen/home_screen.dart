1569ea5becf2cb9abc91846cde08a02dfda33f95
howon-Jeon <howon0830@naver.com>
2025-06-11 15:46:45 +0900

    프로젝트 셋팅

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,15 @@
+// lib 폴더에 폴더 하나 더 만들어 파일 생성
+
+import 'package:flutter/material.dart';
+
+//stless로 자동완성
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
