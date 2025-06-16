f780580ab4d1ed1bb5ed8cfa0c19e35139473970
howon-Jeon <howon0830@naver.com>
2025-06-16 16:26:51 +0900

    프로젝트 셋팅

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:neverland_flutter/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen()
+    ),
+// git test
+  );
+}
