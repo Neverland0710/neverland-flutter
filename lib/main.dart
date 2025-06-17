787068f805b939418e17b89b5a4a918ed4a760d1
김건도 <kimgundo02@naver.com>
2025-06-17 11:59:15 +0900

    chore(main):한국어 날짜 포맷 초기화 로직 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-// import 'package:neverland_flutter/screen/login.dart';
 import 'package:neverland_flutter/screen/main_page.dart';
-void main() {
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // ✅ 비동기 초기화를 위해 필요
+  await initializeDateFormatting('ko');      // ✅ 한국어 날짜 로케일 초기화
   runApp(
-    MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MainPage(),
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
     ),
-// git test
   );
 }
