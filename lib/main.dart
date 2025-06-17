f2f9e13761079c77caf1381a7dd8abca075389cc
김건도 <kimgundo02@naver.com>
2025-06-17 09:45:09 +0900

    feat : 에뮬레이터 시작 임의로 main_page.dart 에서 시작

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:neverland_flutter/screen/login.dart';
-
+// import 'package:neverland_flutter/screen/login.dart';
+import 'package:neverland_flutter/screen/main_page.dart';
 void main() {
   runApp(
     MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: LoginScreen(),
+        home: MainPage(),
     ),
 // git test
   );
