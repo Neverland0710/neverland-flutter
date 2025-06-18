ea88a1d91e84219c1b9c6e731faa3a8d375d4f28
김건도 <kimgundo02@naver.com>
2025-06-19 08:34:01 +0900

    Fix: Java 17 및 Kotlin JVM 버전 충돌 오류 해결
    - build.gradle.kts에서 sourceCompatibility, targetCompatibility 17로 통일
    - kotlinOptions.jvmTarget도 17로 수정
    - Gradle 전체 모듈에 적용되도록 설정 추가
    - Java 8 obsolete warning 제거

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,10 +22,18 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+        options.compilerArgs.add("-Xlint:-options")
+    }
+
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
